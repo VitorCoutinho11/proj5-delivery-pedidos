8d46e8793a27c906855e17e98e80e9a871ea850c
Vitor Silva <vitorofficial11@gmail.com>
2026-06-17 11:42:46 -0300

    prisma correção

--- src/config/prisma.js
+++ src/config/prisma.js
@@ -1,5 +1,18 @@
-const { PrismaClient } = require('../../prisma/generated/client'); 
+const { PrismaClient } = require('../../prisma/generated/client');
 
-const prisma = new PrismaClient();
+let prisma;
 
-module.exports = prisma;
+/**
+ * Inicialização "preguiçosa" (lazy) do PrismaClient.
+ * O client só é criado na primeira vez que alguém acessa uma propriedade (ex: prisma.pedido).
+ * Isso garante que o DATABASE_URL já foi carregado pelo Infisical antes do Prisma tentar usá-lo.
+ */
+module.exports = new Proxy({}, {
+  get(_, prop) {
+    if (!prisma) {
+      prisma = new PrismaClient();
+      console.log("Prisma: Client inicializado com sucesso.");
+    }
+    return prisma[prop];
+  }
+});
